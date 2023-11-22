6a7c4488c74081185042737773f7a1ab144afa69
Patryk Hirny <pathirny@googlemail.com>
2023-11-22 17:31:27 +0000

    added props to form element

--- todo-app/src/components/Form.js
+++ todo-app/src/components/Form.js
@@ -1,10 +1,15 @@
 // declare the form component
-export default function Form() {
+export default function Form(props) {
+  //store the props: addNewTodo function
+  const addNew = props.addTodo;
+  console.log(addNew);
   // return the form element
   return (
     <form>
       <input placeholder="Enter your todo..." type="text" required></input>
-      <button className="addNew">Add new ToDo</button>
+      <button className="addNew" onClick={addNew}>
+        Add new ToDo
+      </button>
     </form>
   );
 }
--- todo-app/src/components/ToDoContainer.js
+++ todo-app/src/components/ToDoContainer.js
@@ -1,7 +1,22 @@
 import Todos from "./ToDos";
 import Form from "./Form";
+import { createClient } from "@supabase/supabase-js";
+
+//store supabase url and key
+const supabaseUrl = "https://tmfrjacuxdesxhbblohq.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRtZnJqYWN1eGRlc3hoYmJsb2hxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA1OTA4NDcsImV4cCI6MjAxNjE2Njg0N30.0PbY1vh8Y0iN_j4-9wm9WsTi0rTf52TWvDB6RZeV-dc";
+const supabase = createClient(supabaseUrl, supabaseKey);
 // declare the ToDos component
 export default function ToDos() {
+  // async function to add new todo
+  const addNewTodo = async () => {
+    // use supabase query to insert new row
+    const { data } = await supabase
+      .from("todos")
+      .insert([{ some_column: "someValue", other_column: "otherValue" }])
+      .select();
+  };
   // this will return all of the todos in a list
   return (
     <main>
@@ -9,7 +24,7 @@ export default function ToDos() {
         {/* individual todos will appear here */}
         <Todos />
       </section>
-      <Form />
+      <Form addTodo={addNewTodo} />
     </main>
   );
 }
