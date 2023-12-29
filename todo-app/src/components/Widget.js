0fb5036f463222ada717c83dd9935315998d4185
Patryk Hirny <pathirny@googlemail.com>
2023-12-29 22:14:07 +0000

    got the access token and song from spotify

--- todo-app/src/components/Widget.js
+++ todo-app/src/components/Widget.js
@@ -3,7 +3,7 @@ export default function Widget() {
   const clientId = "d8a48ebc251748cba27745d97f5cc149"
   const client_secret = "3fe843a2b2304ca897696fbe0a57601f"
   
- const getToken = async () =>{
+  async function getToken(){
       
      const result = await fetch("https://accounts.spotify.com/api/token", {
      method: "POST",   
@@ -18,13 +18,25 @@ export default function Widget() {
      console.log(data)
      const accessToken = data.access_token
      console.log(accessToken)
+
     }
 
     getToken()
 
-  // return (
-  //   <div className="spotifyWidget">
-  //     <h2>Widget</h2>
-  //   </div>
-  // );
+    const songId = '5kGyPEJU3aDeIh2ZC9ZUlI'
+    async function getSong(accessToken){
+      const response = await fetch (`https://api.spotify.com/v1/tracks/${songId}`,{
+      method: 'GET',
+      headers: {'Authorization' : 'Bearer ' + accessToken}
+    })
+
+      const songData = await response.json();
+      console.log(songData)
+    }
+    getSong()
+   return (
+     <div className="spotifyWidget">
+       <h2>Widget</h2>
+     </div>
+   );
 }
