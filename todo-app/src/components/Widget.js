bd68dbe17f82a16e2ec50161302baef251a1482a
Patryk Hirny <pathirny@googlemail.com>
2023-12-29 19:20:33 +0000

    added get songs function

--- todo-app/src/components/Widget.js
+++ todo-app/src/components/Widget.js
@@ -1,4 +1,10 @@
 export default function Widget() {
+
+  const clientId = "d8a48ebc251748cba27745d97f5cc149"
+  async function getSong(){
+
+
+  }
   return (
     <div className="spotifyWidget">
       <h2>Widget</h2>
