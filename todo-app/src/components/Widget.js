75e8cb468f7421d2fa7d59256bce283db1ac6c9b
Patryk Hirny <pathirny@googlemail.com>
2023-12-29 19:49:40 +0000

    wrote fetch function, need the access token

--- todo-app/src/components/Widget.js
+++ todo-app/src/components/Widget.js
@@ -2,9 +2,11 @@ export default function Widget() {
 
   const clientId = "d8a48ebc251748cba27745d97f5cc149"
   async function getSong(){
-
-
+    let accessToken =  localStorage.getItem("access_token")
+    console.log(accessToken)
+    const response = await fetch("https://api.spotify.com/v1/tracks/6DK23fzeG32GDllJRfnDuz")
   }
+  getSong()
   return (
     <div className="spotifyWidget">
       <h2>Widget</h2>
