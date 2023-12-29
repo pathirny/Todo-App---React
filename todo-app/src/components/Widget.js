425de6e0e3469d8b2a9a11fae71f6a7445daa5fd
Patryk Hirny <pathirny@googlemail.com>
2023-12-29 20:39:48 +0000

    attemped another request

--- todo-app/src/components/Widget.js
+++ todo-app/src/components/Widget.js
@@ -1,21 +1,45 @@
 export default function Widget() {
 
-  const client_id = "d8a48ebc251748cba27745d97f5cc149"
+  const clientId = "d8a48ebc251748cba27745d97f5cc149"
   const client_secret = "3fe843a2b2304ca897696fbe0a57601f"
   
-  async function getSong(){
+//   async function getSong(){
     
-    const accessToken = await fetch("https://accounts.spotify.com/api/token", {
-    method: "POST",   
-    headers:{
-        'Authorization': client_id + ':' + client_secret.toString('base64')
-      },
-      body: {
-        grant_type: 'client_credentials'
-      }
-    })
+//     const accessToken = await fetch("https://accounts.spotify.com/api/token", {
+//     method: "POST",   
+//     headers:{
+//         'Authorization': client_id + ':' + client_secret.toString('base64')
+//       },
+//       body: {
+//         grant_type: 'client_credentials'
+//       }
+//     })
+const getRefreshToken = async () => {
 
-console.log(accessToken)
+  // refresh token that has been previously stored
+  const refreshToken = localStorage.getItem('refresh_token');
+  const url = "https://accounts.spotify.com/api/token";
+
+   const payload = {
+     method: 'POST',
+     headers: {
+       'Content-Type': 'application/x-www-form-urlencoded'
+     },
+     body: new URLSearchParams({
+       grant_type: 'refresh_token',
+       refresh_token: refreshToken,
+       client_id: clientId
+     }),
+   }
+   const body = await fetch(url, payload);
+   const response = await body.json();
+console.log(response)
+   localStorage.setItem('access_token', response.accessToken);
+   localStorage.setItem('refresh_token', response.refreshToken);
+ }
+
+ getRefreshToken()
+// console.log(accessToken)
   //   let authOptions ={
   //     url: ,
   //   headers: {
@@ -38,8 +62,6 @@ console.log(accessToken)
     // console.log(accessToken)
     // const response = await fetch("https://api.spotify.com/v1/me")
     // console.log(response)
-  }
-  getSong()
   return (
     <div className="spotifyWidget">
       <h2>Widget</h2>
